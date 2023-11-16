b1f824e2362cb4db633706c1bc3fad8295854639
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-15 21:33:32 -0300

    Usando Format

--- EX02.py
+++ EX02.py
@@ -1,11 +1,7 @@
 nome = input('Digite seu nome: ')
-verde = '\033[0;32m'
 
 # Forma1 #
 print('É um prazer te conhecer,', nome, '!')
 
 # Forma2 #
-print('É um prazer te conhecer, {}!'.format(nome))
-
-# Forma3 com cor#
-print('É um prazer te conhecer, {}{}!'.format(verde, nome))
+print('É um prazer te conhecer, {} !'.format(nome))
