ff0dd081d935ed4a5dec159eda671d5ce67e6f6a
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-15 23:22:36 -0300

    Conversor de Temperatura

---
+++ EX14.py
@@ -0,0 +1,4 @@
+celsius = float(input('Digite a temperatura atual: '))
+print('A temperadura digitada em Celsius é {}ºC\n'
+      'E em Fahrenheit é {}ºF'
+      .format(celsius, ((celsius * 1.8) + 32)))
