a67784ff1fbe475df9d4611e566ae250989f0816
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-15 22:38:44 -0300

    Media de duas notas

---
+++ EX07.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+
+print("A primeira nota é: {}\n"
+      "A segunda nota é: {}\n"
+      "A media é {:.2f}"
+      .format(nota1, nota2, ((nota1 + nota2) / 2)))
