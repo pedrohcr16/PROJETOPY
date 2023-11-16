116b37c5cc003ca45602a16372e1cc223830cf07
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-15 22:48:49 -0300

    Conversor de medidas

---
+++ EX08.py
@@ -0,0 +1,6 @@
+metros = int(input('Insira um valor: '))
+
+print("O valor inserido em metros é {} metros"
+      "\nO valor em centimetros é {} centimetros"
+      "\nO valor em milimetros é {} milimetros"
+      .format(metros, (metros * 100), (metros * 1000)))
