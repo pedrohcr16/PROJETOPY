5189a4b02461a1714c3eb43663b8024c12ad7fb5
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-15 21:47:57 -0300

    Sucessor e Antecessor

---
+++ EX05.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um numero inteiro: '))
+
+print('O numero inserido foi {}\n'
+      'O seu sucessor é {}\n'
+      'O seu antecessor é {}'
+      .format(n1, (n1 + 1), (n1 - 1)))
