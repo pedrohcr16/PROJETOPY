a6f2c5ee11f35b86e42f47c0beb2008174eef701
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-21 23:59:17 -0300

    Dobro, Triplo e Raiz Quadrada

--- EX06.py
+++ EX06.py
@@ -3,4 +3,4 @@ n1 = int(input('Insira um numero: '))
 print('O seu dobro é {}\n'
       'O seu triplo é {}\n'
       'Sua raiz é {:.2f}'
-      .format((n1 * 2), (n1 * 3), (n1 ** (1/2))))
+      .format((n1 * 2), (n1 * 3), (n1 ** (1 / 2))))
