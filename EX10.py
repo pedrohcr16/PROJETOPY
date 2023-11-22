63d07fa87d542218c9429860cf783d2f4569f1ab
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-21 23:37:35 -0300

    Conversor de moeda

--- EX10.py
+++ EX10.py
@@ -1,4 +1,4 @@
-carteira = int(input('Insira quantos reais você tem na carteira: '))
+carteira = int(input('Digite quantos reais você tem na carteira: '))
 conversao = (carteira / 4.86)
 print('Com esse valor de R${:.2f} você pode comprar {:.2f} U$'
       .format(carteira, conversao))
