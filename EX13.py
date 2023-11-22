839a3d49595874cf351a340f10feec76178a15c8
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-21 23:28:33 -0300

    Reajuste Salarial

--- EX13.py
+++ EX13.py
@@ -1,4 +1,4 @@
 salario = float(input('Digite o seu salário: '))
-print('O seu salário é de {:.2f} R$\n'
+print('O seu salário é {:.2f} R$\n'
       'Com aumento de 15% vai para {:.2f} R$'
       .format(salario, (salario + (salario * 0.15))))
