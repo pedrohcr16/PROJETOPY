87e1f9a91b91ecfaa79d3044aaefb736e71aaf0d
pedrohcr16 <pedrophcr16@gmail.com>
2023-11-21 23:19:21 -0300

    Aluguel de Carro

--- EX15.py
+++ EX15.py
@@ -5,7 +5,7 @@ print('Você percorreu com o carro por {} km\n'
       'O valor por km rodado é de 0,15 R$\n'
       'Portanto o valor total a pagar pelos km rodados é {} R$\n'
       'Você utilizou o carro por {} dias\n'
-      'O valor por dia utilizado é de 60.0 R$\n'
+      'O valor por dia utilizado é de 60,0 R$\n'
       'Portanto o valor total a pagar pelos dias utilizados é {} R$\n'
       'O total a pagar é de {} R$'
       .format(km, km*0.15, dia, dia*60, ((km*0.15) + (dia*60))))
