654eddb07c611d3e05ec86979ec2bf7901b4b80d
pedrohcr16 <pedrophcr16@gmail.com>
2023-12-18 23:48:54 -0300

    O maior e menor valor

---
+++ EX33.py
@@ -0,0 +1,6 @@
+n1 = int(input('Insira o primeiro numero: '))
+n2 = int(input('Insira o segundo numero: '))
+n3 = int(input('Insira o terceiro numero: '))
+
+print('O maior é o:', max(n1, n2, n3))
+print('O menor é o:', min(n1, n2, n3))
