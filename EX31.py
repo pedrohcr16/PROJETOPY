264ca8d5903edaf642e049c66cb9518c8da562c0
pedrohcr16 <pedrophcr16@gmail.com>
2023-12-18 23:44:44 -0300

    Custo da viagem

--- EX31.py
+++ EX31.py
@@ -6,4 +6,3 @@ if distancia <= 200:
 else:
     print('A distancia percorrida é de {} KM\n'
           'O valor a ser cobrado é de {:.2f} R$'.format(distancia, distancia * 0.45))
-print('Fim do Programa')
