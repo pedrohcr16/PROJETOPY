22211b70075b8b266cc881e80a51af3654ef1849
pedrohcr16 <pedrophcr16@gmail.com>
2024-01-22 22:00:46 -0300

    Tabuada v.2

---
+++ EX49.py
@@ -0,0 +1,4 @@
+multiplicando = int(input('Digite o valor do multiplicando: '))
+
+for i in range(1, 11):
+    print('O valor total da tabuada de {} x {} = {}'.format(multiplicando, i, multiplicando * i))
