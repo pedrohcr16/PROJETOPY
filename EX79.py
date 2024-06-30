fa93eb3c6b735b33961d425bfff30e75e303346b
pedrohcr16 <pedrophcr16@gmail.com>
2024-06-30 12:24:55 -0300

    Valores únicos em uma Lista

---
+++ EX79.py
@@ -0,0 +1,15 @@
+op = ''
+lista = []
+while op != "N":
+    insercao = int(input('Digite um número: '))
+    op = str(input('Quer continuar ? [Y/N]: ')).upper().strip()
+    if insercao in lista:
+        del insercao
+        print('Valor duplicado não adicionado!')
+        print('=-'*20)
+    else:
+        lista.append(insercao)
+        print('Valor adicionado com sucesso!')
+        print('=-'*20)
+    print(sorted(lista))
+print('Fim do Programa!')
