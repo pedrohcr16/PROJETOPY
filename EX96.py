56460a751148b343c40d9e08a7ee7f4e556cce00
pedrohcr16 <pedrophcr16@gmail.com>
2024-08-05 22:10:43 -0300

    Função que calcula área

---
+++ EX96.py
@@ -0,0 +1,11 @@
+def area():
+    print('Controle de Terrenos')
+    print('-' * 45)
+    largura = float(input('Digite a largura: '))
+    comprimento = float(input('Digite a comprimento: '))
+    print(f'A área de um terreno de {largura} x {comprimento} = {largura * comprimento}m²')
+    print('-' * 45)
+
+
+area()
+
